ec93635f43da3ca5eb856d633e8549d60ae79b51
RiccoSoares <52585330+RiccoSoares@users.noreply.github.com>
2020-08-12 20:39:03 -0300

    Add files via upload

--- classification/prepare_folds.py
+++ classification/prepare_folds.py
@@ -41,6 +41,8 @@ print(covid_p)
 print('')
 
 all_patients = other_patients + covid_patients
+all_covid_patients = covid_patients.copy()
+all_other_patients = other_patients.copy()
 
 print('Total number of patients in one of the two classes: ', 
 	      len(all_patients))
@@ -55,19 +57,25 @@ validation_folds = []
 #defining lists of validation folds by class for sanity check
 covid_validation_folds = []
 other_validation_folds = []
-for i in range(1,5): #select each train and validation fold
+for i in range(1,6): #select each train and validation fold
+	if i == 5:
+		covid_range = 17
+		other_range = 12
+	else:
+		covid_range = 14
+		other_range = 10
 	validation = []
 	train = []
 	covid_validation = []
 	other_validation = []
 #selecting 14 covid patients to validate
-	for i in range(14):
+	for i in range(covid_range):
 		random_int = random.randint(0, len(covid_patients) - 1)
 		validation.append(covid_patients[random_int])
 		covid_validation.append(covid_patients[random_int])
 		covid_patients.pop(random_int)
 #selecting 10 non covid patients to validate
-	for i in range(10):
+	for i in range(other_range):
 		random_int = random.randint(0, len(other_patients) - 1)
 		validation.append(other_patients[random_int])
 		other_validation.append(other_patients[random_int])
@@ -79,31 +87,7 @@ for i in range(1,5): #select each train and validation fold
 	train_folds.append(train)
 	covid_validation_folds.append(covid_validation)
 	other_validation_folds.append(other_validation)
-	
-#defining the last fold
-validation = []
-train = []
-covid_validation = []
-other_validation = []
-#selecting 17 covid patients
-for i in range(17):
-	random_int = random.randint(0, len(covid_patients) - 1)
-	validation.append(covid_patients[random_int])
-	covid_validation.append(covid_patients[random_int])
-	covid_patients.pop(random_int)
-#selecting 12 covid patients
-for i in range(12):
-	random_int = random.randint(0, len(other_patients) - 1)
-	validation.append(other_patients[random_int])
-	other_validation.append(other_patients[random_int])
-	other_patients.pop(random_int)
-#appending the validation and training folds into the final list
-	train = list(set(all_patients) - set(covid_validation))
-	train = list(set(train) - set(other_validation))
-	validation_folds.append(validation)
-	train_folds.append(train)
-	covid_validation_folds.append(covid_validation)
-	other_validation_folds.append(other_validation)
+
 	
 print('patients not used in any validation folder:', covid_patients+other_patients) #should print an empty list
 print('printing folds: ')
@@ -115,9 +99,40 @@ for i in range(5):
 	print('Train Fold number ', str(i+1), ':', ['C' + str(p) for p in ordered_list])
 	print('')
 
-print('Sanity Check: ')
+test_list = []
+print('Printing Validation Folds per class: ')
 for i in range(5):
-	l = covid_validation_folds[i] + other_validation_folds[i]
-	ordered_l = sorted([int(p[1:]) for p in l])
-	print('This should be equal to the validation Fold number ', str(i+1), ':', ['C' + str(p) for p in ordered_l])
+	test_list.extend(covid_validation_folds[i])
+	test_list.extend(other_validation_folds[i])
+	ordered_l = sorted([int(p[1:]) for p in covid_validation_folds[i]])
+	print('covid validation fold ', str(i+1), ':', ['C' + str(p) for p in ordered_l])
+	ordered_l = sorted([int(p[1:]) for p in other_validation_folds[i]])
+	print('other validation fold ', str(i+1), ':', ['C' + str(p) for p in ordered_l])
+
+if len(set(all_patients) - set(test_list))>0:
+	print('error')
+	
+print('Printing Test Folds per class: ')
+for i in range(5):
+	train_covid = []
+	train_other = []
+	train_fold = train_folds[i]
+	#verifying if any of the patients included in validation fold is in trian fold too
+	for valid_item in validation_folds[i]:
+		for train_item in train_fold:
+			if valid_item == train_item:
+				print('error occured with patient: ', train_item)
+	for train_patient in train_fold:
+		found = False
+		for other_patient in all_other_patients:
+			if train_patient == other_patient:
+				found = True
+				train_other.append(train_patient)
+		if not found:
+			train_covid.append(train_patient)
+			
+	ordered_l = sorted([int(p[1:]) for p in train_covid])
+	print('covid train fold ', str(i+1), ':', ['C' + str(p) for p in ordered_l])
+	ordered_l = sorted([int(p[1:]) for p in train_other])
+	print('other train fold ', str(i+1), ':', ['C' + str(p) for p in ordered_l])
 	
