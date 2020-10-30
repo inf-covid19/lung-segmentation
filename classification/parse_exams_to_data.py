69902cb76c48443f34133c10fed97020fbeffb91
Francisco Bento <franciscobdsjunior@gmail.com>
2020-10-30 10:49:26 -0300

    Fix script to cidia-server

--- classification/parse_exams_to_data.py
+++ classification/parse_exams_to_data.py
@@ -8,24 +8,32 @@ import pandas as pd
 
 
 EXAM_FOLDERS = [
-    '/home/chicobentojr/Desktop/cidia-files/exams',
-    '/home/chicobentojr/Desktop/cidia-files/exams/C11'
+    '/ssd/share/CT-Original/DICOM-HCPA/exame-pulmao',
+    '/ssd/share/CT-Original/DICOM-HMV/exame-pulmao',
+    '/home/users/chicobentojr/workspace/HCPA-organizado-parte-2',
+
 ]
 VIDEO_URL_PREFIX = '/assets/exam-data/preview-video/'
 VIDEO_FOLDERS = [
-    '/home/chicobentojr/Desktop/cidia-files/MP4-HMV-HCPA',
+    '/ssd/share/CT-Segmentado/03-P-HNN/MP4-HMV-HCPA',
 ]
 SLICES_FOLDERS = [
-    '/home/chicobentojr/Desktop/cidia-files/important-slices',
+    '/ssd/share/CT-Segmentado/03-P-HNN/NII-HCPA/exame-pulmao/',
+    '/ssd/share/CT-Segmentado/03-P-HNN/NII-HMV/exame-pulmao/',
+    '/ssd/share/CT-Segmentado/03-P-HNN/NII-HCPA-parte-2/exame-pulmao/',
 ]
 SLICE_PREFIX = '/assets/exam-data/important-slices/'
 
-RESULT_CSV_FILE = 'fapergs.csv'
+RESULT_CSV_FILE = 'fapergs3.csv'
 RESULT_COLUMNS = {
     "Radiology ": "Radiology",
+    "AVG ALL": "Avegare All",
+    "1: Spherical": "Spherical",
     "2: Slices 2D": "Slices 2D",
     "3: VR": "Volume Rendering",
-    "AVG 2 e 3": "Average",
+    "AVG 2 e 3": "Average 2-3",
+    "AVG 1 e 2": "Average 1-2",
+    "AVG 1 e 3": "Average 1-3",
     # "Fold number": 2,
     # "Patient ID": "C114",
     # "Img. esf\u00e9ricas": "2,79%",
@@ -44,7 +52,26 @@ RESULT_COLUMNS = {
 }
 
 EXAM_NAME_MAP = {
-    "EXAME11": "C11"
+    "EXAME1": "C1",
+    "EXAME2": "C2",
+    "EXAME3": "C3",
+    "EXAME4": "C4",
+    "EXAME5": "C5",
+    "EXAME6": "C6",
+    "EXAME7": "C7",
+    "EXAME8": "C8",
+    "EXAME9": "C9",
+    "EXAME10": "C10",
+    "EXAME11": "C11",
+    "EXAME12": "C12",
+    "EXAME13": "C13",
+    "EXAME14": "C14",
+    "EXAME15": "C15",
+    "EXAME16": "C16",
+    "EXAME17": "C17",
+    "EXAME18": "C18",
+    "EXAME19": "C19",
+    "EXAME20": "C20",
 }
 
 
@@ -139,10 +166,12 @@ for key, patient in result_dict.items():
 
     patient_id = patient['patientID']
 
-    for video_folder in SLICES_FOLDERS:
-        for root, dirnames, filenames in os.walk(video_folder):
+    for slice_folder in SLICES_FOLDERS:
+        for root, dirnames, filenames in os.walk(slice_folder):
             important_slices = [
-                f for f in filenames if patient_id == root.split('/')[-1]]
+                f for f in filenames
+                if (patient_id == root.split('/')[-1] or patient_id == root.split('/')[-2])
+                and ".png" in f]
 
             if important_slices:
                 print(f"Slices found to {patient_id}: {important_slices}")
@@ -156,7 +185,7 @@ for key, patient in result_dict.items():
                     shutil.copy(f"{root}/{slc}", f"{pat_slc_folder}/{slc}")
 
                 important_slices = [
-                    f"{SLICE_PREFIX}{x}" for x in important_slices]
+                    f"{SLICE_PREFIX}{patient_id}/{x}" for x in important_slices]
                 patient['importantSlices'] = important_slices
 print()
 
